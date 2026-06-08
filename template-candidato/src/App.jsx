584df17e9afbae38e41a835ec7f284b0f0195106
lucas3lima <lucaslima.code@gmail.com>
2026-06-08 19:16:25 -0300

    first commit

---
+++ template-candidato/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { supabase } from './supabaseClient';
+
+function App() {
+  return (
+    <h1 className='text-3xl font-bold underline'>Hello World</h1>
+  );
+}
+
+export default App;
